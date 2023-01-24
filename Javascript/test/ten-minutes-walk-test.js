80e407772a2601f4a1f6b2a1219cd01390432bda
Stefan Talbot <satalbot@protonmail.com>
2023-01-23 22:50:53 -0500

    Test: Add Test for ten-minutes-walk.js

---
+++ Javascript/test/ten-minutes-walk-test.js
@@ -0,0 +1,11 @@
+import isValidWalk from '../ten-minutes-walk.js';
+import { assert } from 'chai';
+
+describe("digitalRoot", () => {
+  it("should return the recursive sum of all the digits in a number.", () => {
+    assert.isTrue(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'should return true');
+    assert.isFalse(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'should return false');
+    assert.isFalse(isValidWalk(['w']), 'should return false');
+    assert.isFalse(isValidWalk(['n','n','n','s','n','s','n','s','n','s']), 'should return false');
+  });
+});
