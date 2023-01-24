ec236fcb9b57c27530fd7ea4e0d69493a2a574e3
Stefan Talbot <satalbot@protonmail.com>
2023-01-23 22:52:41 -0500

    Refactor: Change Test Description

--- Javascript/test/ten-minutes-walk-test.js
+++ Javascript/test/ten-minutes-walk-test.js
@@ -1,8 +1,8 @@
 import isValidWalk from '../ten-minutes-walk.js';
 import { assert } from 'chai';
 
-describe("digitalRoot", () => {
-  it("should return the recursive sum of all the digits in a number.", () => {
+describe("tenMinutesWalk", () => {
+  it("should return true, if given directions takes 10 minutes and ends at the same coordinates, false other wise.", () => {
     assert.isTrue(isValidWalk(['n','s','n','s','n','s','n','s','n','s']), 'should return true');
     assert.isFalse(isValidWalk(['w','e','w','e','w','e','w','e','w','e','w','e']), 'should return false');
     assert.isFalse(isValidWalk(['w']), 'should return false');
