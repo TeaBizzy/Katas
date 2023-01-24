f69ae025ef4c5a072ff816d290921844ce1e739a
Stefan Talbot <satalbot@protonmail.com>
2023-01-23 22:36:40 -0500

    Feat: Complete Kata
    feat: complete kata, digital-root. Returns the recursive sum of all the digits in a number.

---
+++ Javascript/digital-root.js
@@ -0,0 +1,8 @@
+export default function digitalRoot(n) {
+  const numArr = n.toString()
+    .split('')
+    .map(n => Number(n))
+
+  const reducedNum = numArr.reduce((acc, val) => acc + val)
+  return reducedNum >= 10 ? digitalRoot(reducedNum) : reducedNum
+}
