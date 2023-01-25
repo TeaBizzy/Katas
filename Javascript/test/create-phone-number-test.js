3cf334e30109732c40f661334bed223a8aade990
Stefan Talbot <satalbot@protonmail.com>
2023-01-25 14:38:46 -0500

    Test: Add test for phone-number.js

---
+++ Javascript/test/create-phone-number-test.js
@@ -0,0 +1,10 @@
+import createPhoneNumber from '../src/create-phone-number.js';
+import { assert } from 'chai';
+
+describe("Create Phone Number", () => {
+  it("Fixed tests", () => {
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+    assert.strictEqual(createPhoneNumber([1, 1, 1, 1, 1, 1, 1, 1, 1, 1]), "(111) 111-1111");
+    assert.strictEqual(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]), "(123) 456-7890");
+  });
+});
